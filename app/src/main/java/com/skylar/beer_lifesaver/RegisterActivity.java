98c0186880ea008a3b742d08aa17a9b65432bf55
Lorenah <lorayne911@gmail.com>
2021-01-19 18:34:12 +0300

    created home page for searching styled beers

--- app/src/main/java/com/skylar/beer_lifesaver/RegisterActivity.java
+++ app/src/main/java/com/skylar/beer_lifesaver/RegisterActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
