067ed43f6dd9c71c8850981ef8a4d08cd4997474
Lorenah <lorayne911@gmail.com>
2021-01-20 05:15:07 +0300

    Add constants class

---
+++ app/src/main/java/com/skylar/beer_lifesaver/Constants.java
@@ -0,0 +1,6 @@
+package com.skylar.beer_lifesaver;
+
+public class Constants {
+    public static final String SANDBOX_BASE_URL = "https://sandbox-api.brewerydb.com/v2/";
+    public static final String SANDBOX_KEY = BuildConfig.SANDBOX_KEY;
+}
